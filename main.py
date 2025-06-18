b82a9384e10bd586b5d49e58735d515711ce23b8
JoaoAASilva <araujojoaoarthur31@gmail.com>
2025-06-18 08:43:12 -0300

    upd 0.1
    atualizaçao- site tem funcionando  sitema de custumizaçao do perfil e funçao add ou posta musica fui  adicionada   e  nova funçao caixa de mensagem rodara em breve
    
    e aba artista sera modificada  na proxima upd

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = 'uploads'
+app.config['MAX_CONTENT_LENGTH'] = 2 * 1024 * 1024  # 2MB
 
-# Importa as rotas depois que o app foi criado
+# Cria a pasta de uploads se não existir
+os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
+
 from views import *
 
 if __name__ == "__main__":
--- views.py
+++ views.py
@@ -1,6 +1,24 @@
+from flask import render_template, request, url_for
 from main import app
-from flask import render_template
+import os
+from werkzeug.utils import secure_filename
 
-@app.route("/")
+@app.route("/", methods=["GET"])
 def homepage():
     return render_template("homepage.html")
+
+@app.route("/customizar", methods=["POST"])
+def customizar():
+    nome = request.form.get("nome")
+    cor = request.form.get("cor") or "#ffffff"
+
+    foto = request.files.get("foto")
+    if foto and foto.filename:
+        filename = secure_filename(foto.filename)
+        caminho = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        foto.save(caminho)
+        foto_url = url_for('static', filename=f'../uploads/{filename}')
+    else:
+        foto_url = url_for('static', filename='img/perfil_padrao.jpg')
+
+    return render_template("homepage.html", nome=nome, foto_url=foto_url, cor=cor)
